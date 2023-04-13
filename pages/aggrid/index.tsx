5f04b5e4e5f28a68143f23da458237433b304e8e
Jesray Garciano <jesraygarciano@Jesrays-MacBook-Pro.local>
2023-04-13 11:11:19 +0800

    Added Grouprows component

--- pages/aggrid/index.tsx
+++ pages/aggrid/index.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import AgGridGrouping from "../../components/AgGridGrouping/AgGridGrouping";
+import GroupRows from "../../components/GroupRows/GroupRows";
 
 export default function AgGrid() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-xl mb-4">AG Grid Row Grouping Example</h1>
+      <h1 className="text-xl mb-4">NOTION like AG Grid Row Grouping</h1>
+      <GroupRows />
+
+      <h1 className="text-xl mb-4 mt-[5rem]">AG Grid Row Grouping Example</h1>
       <AgGridGrouping />
     </main>
   );
